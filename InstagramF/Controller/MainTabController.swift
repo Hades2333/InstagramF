5e3e5c05db62c10a89b3a7f62ed27cada6d53bb2
Nikolai Beklemishchev <greenavalonbarrel23@gmail.com>
2021-04-07 12:27:11 +0300

    set up project directory's

---
+++ InstagramF/Controller/MainTabController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabController.swift
+//  InstagramF
+//
+//  Created by Hellizar on 7.04.21.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+    
+}
