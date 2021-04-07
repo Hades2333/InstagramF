e1bc599a6e3e95dc6aa7514f7e755754092d8a05
Nikolai Beklemishchev <greenavalonbarrel23@gmail.com>
2021-04-07 16:12:51 +0300

    creating custom subclasses

--- InstagramF/Controller/Authentication/LoginController.swift
+++ InstagramF/Controller/Authentication/LoginController.swift
@@ -17,28 +17,13 @@ class LoginController: UIViewController {
     }()
 
     private let emailTextField: UITextField = {
-        let textField = UITextField()
-        textField.borderStyle = .none
-        textField.textColor = .white
-        textField.keyboardAppearance = .dark
+        let textField = CustomTextField(placeholder: "Email")
         textField.keyboardType = .emailAddress
-        textField.backgroundColor = UIColor(white: 1, alpha: 0.1)
-        textField.setHeight(50)
-        textField.attributedPlaceholder = NSAttributedString(string: "Email",
-                                                             attributes: [.foregroundColor: UIColor(white: 1, alpha: 0.7)])
         return textField
     }()
 
     private let passwordTextField: UITextField = {
-        let textField = UITextField()
-        textField.borderStyle = .none
-        textField.textColor = .white
-        textField.keyboardAppearance = .dark
-        textField.keyboardType = .emailAddress
-        textField.backgroundColor = UIColor(white: 1, alpha: 0.1)
-        textField.setHeight(50)
-        textField.attributedPlaceholder = NSAttributedString(string: "Password",
-                                                             attributes: [.foregroundColor: UIColor(white: 1, alpha: 0.7)])
+        let textField = CustomTextField(placeholder: "Password")
         textField.isSecureTextEntry = true
         return textField
     }()
@@ -53,33 +38,19 @@ class LoginController: UIViewController {
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
         return button
     }()
-
+    
     private let dontHaveAccountButton: UIButton = {
         let button = UIButton(type: .system)
-
-        let atts: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(white: 1, alpha: 0.7), .font: UIFont.systemFont(ofSize: 16)]
-        let boldAtts: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(white: 1, alpha: 0.7), .font: UIFont.boldSystemFont(ofSize: 16)]
-
-        let attributedTitle = NSMutableAttributedString(string: "Don't have an account?  ", attributes: atts)
-        attributedTitle.append(NSAttributedString(string: "Sign Up", attributes: boldAtts))
-
-        button.setAttributedTitle(attributedTitle, for: .normal)
+        button.attributedTitle(firstPart: "Don't have an account?  ", secondPart: "Sign Up")
         return button
     }()
-
+    
     private let forgotPasswordButton: UIButton = {
         let button = UIButton(type: .system)
-
-        let atts: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(white: 1, alpha: 0.7), .font: UIFont.systemFont(ofSize: 16)]
-        let boldAtts: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(white: 1, alpha: 0.7), .font: UIFont.boldSystemFont(ofSize: 16)]
-
-        let attributedTitle = NSMutableAttributedString(string: "Forgot yout password?  ", attributes: atts)
-        attributedTitle.append(NSAttributedString(string: "Get help signing in", attributes: boldAtts))
-
-        button.setAttributedTitle(attributedTitle, for: .normal)
+        button.attributedTitle(firstPart: "Forgot password?  ", secondPart: "Get help signing in")
         return button
     }()
-
+    
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
