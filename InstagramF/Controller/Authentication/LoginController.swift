69d6fcc9c9d8d1b7f03b0d508422cc1d8ebbaa8b
Nikolai Beklemishchev <greenavalonbarrel23@gmail.com>
2021-04-07 16:32:16 +0300

    registration page UI

--- InstagramF/Controller/Authentication/LoginController.swift
+++ InstagramF/Controller/Authentication/LoginController.swift
@@ -38,19 +38,20 @@ class LoginController: UIViewController {
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
         return button
     }()
-    
+
     private let dontHaveAccountButton: UIButton = {
         let button = UIButton(type: .system)
         button.attributedTitle(firstPart: "Don't have an account?  ", secondPart: "Sign Up")
+        button.addTarget(self, action: #selector(handleShowSignUp), for: .touchUpInside)
         return button
     }()
-    
+
     private let forgotPasswordButton: UIButton = {
         let button = UIButton(type: .system)
         button.attributedTitle(firstPart: "Forgot password?  ", secondPart: "Get help signing in")
         return button
     }()
-    
+
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -58,17 +59,17 @@ class LoginController: UIViewController {
         configureUI()
     }
 
+    //MARK: - Actions
+    @objc func handleShowSignUp() {
+        let controller = RegistrationController()
+        navigationController?.pushViewController(controller, animated: true)
+    }
+
     //MARK: - Helpers
     func configureUI() {
-        view.backgroundColor = .white
+        configureGradientLayer()
         navigationController?.navigationBar.isHidden = true
         navigationController?.navigationBar.barStyle = .black
-
-        let gradient = CAGradientLayer()
-        gradient.colors = [UIColor.systemPurple.cgColor, UIColor.systemBlue.cgColor]
-        gradient.locations = [0, 1]
-        view.layer.addSublayer(gradient)
-        gradient.frame = view.frame
 
         view.addSubview(iconImage)
         iconImage.centerX(inView: view)
@@ -87,7 +88,5 @@ class LoginController: UIViewController {
         view.addSubview(dontHaveAccountButton)
         dontHaveAccountButton.centerX(inView: view)
         dontHaveAccountButton.anchor(bottom: view.safeAreaLayoutGuide.bottomAnchor)
-
-
     }
 }
--- InstagramF/Controller/Authentication/RegistrationController.swift
+++ InstagramF/Controller/Authentication/RegistrationController.swift
@@ -10,10 +10,78 @@ import UIKit
 class RegistrationController: UIViewController {
 
     //MARK: - Properties
+    private let plusPhotoButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "plus_photo"), for: .normal)
+        button.tintColor = .white
+        return button
+    }()
+
+    private let emailTextField: UITextField = {
+        let textField = CustomTextField(placeholder: "Email")
+        textField.keyboardType = .emailAddress
+        return textField
+    }()
+
+    private let passwordTextField: UITextField = {
+        let textField = CustomTextField(placeholder: "Password")
+        textField.isSecureTextEntry = true
+        return textField
+    }()
+
+    private let fullNameTextField = CustomTextField(placeholder: "Fullname")
+    private let usernameTextField = CustomTextField(placeholder: "Username")
+
+    private let signUpButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Sign Up", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)
+        button.layer.cornerRadius = 5
+        button.setHeight(50)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        return button
+    }()
+
+    private let alreadyHaveAccountButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.attributedTitle(firstPart: "Already have an account?  ", secondPart: "Sign Up")
+        button.addTarget(self, action: #selector(handleShowLogin), for: .touchUpInside)
+        return button
+    }()
 
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPink
+        configureUI()
+    }
+
+    //MARK: - Actions
+    @objc func handleShowLogin() {
+        navigationController?.popViewController(animated: true)
+    }
+
+    //MARK: - Helpers
+    func configureUI() {
+        configureGradientLayer()
+
+        view.addSubview(plusPhotoButton)
+        plusPhotoButton.centerX(inView: view)
+        plusPhotoButton.setDimensions(height: 140, width: 140)
+        plusPhotoButton.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 32)
+
+        let stack = UIStackView(arrangedSubviews: [emailTextField, passwordTextField,
+                                                   fullNameTextField, usernameTextField,
+                                                   signUpButton])
+        stack.axis = .vertical
+        stack.spacing = 20
+
+        view.addSubview(stack)
+        stack.anchor(top: plusPhotoButton.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor,
+                     paddingTop: 32, paddingLeft: 32, paddingRight: 32)
+
+        view.addSubview(alreadyHaveAccountButton)
+        alreadyHaveAccountButton.centerX(inView: view)
+        alreadyHaveAccountButton.anchor(bottom: view.safeAreaLayoutGuide.bottomAnchor)
     }
 }
