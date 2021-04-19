6935c0a9ab8aa73172408044c9f79c612b8a61cd
Nikolai Beklemishchev <greenavalonbarrel23@gmail.com>
2021-04-19 16:00:33 +0300

    notification controller ui

--- InstagramF/View/FeedCell.swift
+++ InstagramF/View/FeedCell.swift
@@ -10,6 +10,7 @@ import UIKit
 protocol FeedCellDelegate: class {
     func cell(_ cell: FeedCell, wantsToShowCommentsFor post: Post)
     func cell(_ cell: FeedCell, didLikePost post: Post)
+    func cell(_ cell: FeedCell, wantsToShowProfileFor uid: String)
 }
 
 class FeedCell: UICollectionViewCell {
@@ -22,12 +23,17 @@ class FeedCell: UICollectionViewCell {
         didSet { configure() }
     }
 
-    private let profileImageView: UIImageView = {
+    private lazy var profileImageView: UIImageView = {
         let iv = UIImageView()
         iv.contentMode = .scaleAspectFill
         iv.clipsToBounds = true
         iv.isUserInteractionEnabled = true
         iv.backgroundColor = .lightGray
+
+        let tap = UITapGestureRecognizer(target: self, action: #selector(showUserProfile))
+        iv.isUserInteractionEnabled = true
+        iv.addGestureRecognizer(tap)
+
         return iv
     }()
 
@@ -35,7 +41,7 @@ class FeedCell: UICollectionViewCell {
         let button = UIButton(type: .system)
         button.setTitleColor(.black, for: .normal)
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 13)
-        button.addTarget(self, action: #selector(didTapUsername), for: .touchUpInside)
+        button.addTarget(self, action: #selector(showUserProfile), for: .touchUpInside)
         return button
     }()
 
@@ -107,8 +113,9 @@ class FeedCell: UICollectionViewCell {
     }
 
     //MARK: - Actions
-    @objc func didTapUsername() {
-
+    @objc func showUserProfile() {
+        guard let viewModel = viewModel else { return }
+        delegate?.cell(self, wantsToShowProfileFor: viewModel.post.ownerUid)
     }
 
     @objc func didTapComments() {
