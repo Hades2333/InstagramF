9142e22e01df51544805f0d8e2caf756396a76f7
Nikolai Beklemishchev <greenavalonbarrel23@gmail.com>
2021-04-19 16:15:22 +0300

    notification data model

---
+++ InstagramF/Model/Notification.swift
@@ -0,0 +1,40 @@
+//
+//  Notification.swift
+//  InstagramF
+//
+//  Created by Hellizar on 19.04.21.
+//
+
+import Firebase
+
+enum NotificationType: Int {
+    case like
+    case follow
+    case comment
+
+    var notificationMessage: String {
+        switch self {
+        case .like: return " liked your post"
+        case .comment: return " commented on yout post"
+        case .follow: return " started following you"
+        }
+    }
+}
+
+struct Notification {
+    let uid: String
+    var postImageUrl: String?
+    var postId: String?
+    let timestamp: Timestamp
+    let type: NotificationType
+    let id: String
+
+    init(dictionaty: [String: Any]) {
+        self.timestamp = dictionaty["timestamp"] as? Timestamp ?? Timestamp(date: Date())
+        self.id = dictionaty["id"] as? String ?? ""
+        self.uid = dictionaty["uid"] as? String ?? ""
+        self.postId = dictionaty["postId"] as? String ?? ""
+        self.postImageUrl = dictionaty["postImageUrl"] as? String ?? ""
+        self.type = NotificationType(rawValue: dictionaty["type"] as? Int ?? 0) ?? .like
+    }
+}
